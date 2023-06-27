132190d23cc79f2e19b02980a0643b2cc015fdeb
HIGOR J. S. CAMPOS <hh1217higorcampos@hotmail.com>
2023-06-27 16:31:11 -0300

    Update moser_language.js

--- languages/moser_language.js
+++ languages/moser_language.js
@@ -17,7 +17,14 @@ function English() {
     Escrever("li-titulo", "SELECT METHOD")
 
     //SEÇÃO 1
-    const paragrafo_explicacao = ""
+    const paragrafo_explicacao = "The Moser (1996) method for selecting underground mine access was developed over several projects of hard rock mines around the world."
++"The proposed model does not replace detailed feasibility studies, but aims to significantly reduce the amount of work in the early stages of mining planning.<br>"
++"<strong>Notes:</strong><br>"
++"- Moser did not take into account any data from soft rock mining operations.<br>"
++"- The author’s proposed limits for each parameter have been surpassed by advancements in technology.<br>"
++"- The model does not consider the use of inclined belt conveyors as a viable option for transporting ore.<br><br>"
++"<strong>References:</strong><br>"
++"MOSER, P. Primary development of underground hard rock mines. In: SYMPOSIUM ON MINE PLANNING AND EQUIPMENT SELECTION. 1996. São Paulo. Proceedings […]. São Paulo, 1996. p. 31-3."
 
     Escrever("paragrafo-explicacao", paragrafo_explicacao)
 
